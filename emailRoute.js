80fea5162ea84fcc313b2943388b84edf238232b
William Li <williamli@bbi.io>
2019-04-18 11:44:51 +0800

    clean up

--- emailRoute.js
+++ emailRoute.js
@@ -1,11 +1,8 @@
-const url = require('url');
 const md5 = require('md5');
 
 module.exports = (req, res) => {
   const status = (process.env.ENV === 'production') ? 301 : 302;
   const url_parts = req.url.split('/');
-  console.log(url_parts);
-  // const email = url_parts.query.q.trim().toLowerCase();
   const email = url_parts[2].trim().toLowerCase();
   const size = url_parts[3] ? `/${url_parts[3]}` : '';
 
